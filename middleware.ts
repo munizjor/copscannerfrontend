487c42a60e7f7cd6ff74d2fc2bf8de897c96f359
Jorge Muniz <munizjor@gmail.com>
2025-06-21 12:08:12 -0400

    update middleware redirect.

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
-export function middleware(request: NextRequest) {
-  const host = request.headers.get('host');
-  console.log('Detected host header:', host);
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export function middleware(request: NextRequest) {  
+  const host = request.headers.get('host')
   if (host === 'copscannerfrontend.onrender.com') {
-    return NextResponse.redirect('https://copscanneralert.westnode.com', 308);
+    console.log('REDIRECTING!')
+    return NextResponse.redirect('https://copscanneralert.westnode.com')
   }
-  return NextResponse.next();
+  
+  return NextResponse.next()
 }
