d7f4c83972e6e385ebd88c5ac144607435d5ed86
Jorge Muniz <munizjor@gmail.com>
2025-06-23 23:29:50 -0400

    Update middleware.ts
    update redirect to https://sherlockiq.com/

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const host = request.headers.get('host')
   if (host === 'copscannerfrontend.onrender.com') {
     console.log('REDIRECTING!')
-    return NextResponse.redirect('https://copscanneralert.westnode.com')
+    return NextResponse.redirect('https://sherlockiq.com')
   }
   
   return NextResponse.next()
